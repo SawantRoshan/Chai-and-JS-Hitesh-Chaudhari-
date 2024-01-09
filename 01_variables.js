ee925d1324fa4215d1baf64f2ea0c084239162da
Roshan <sawantroshan98@gmail.com>
2024-01-09 11:23:21 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+const accId = 101;
+let accName = "Roshan";
+var accEmail = "roshan@gmail.com";
+password = "101Rs"
+let accState;
+/* prefer not to use var
+because of functional scope and block scope issues */
+console.table([accId, accName, accEmail, password, accState])
---
+++ 02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer vaersion
+
+let name = "Roshan";
+let age = 25;
+let coder = true;
+
+// Premitive Data types
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Null  //typeof(null) => object
+// 5.Undefined //typeof(null) => Undefined
+
+//Non-Premitive data types
+// 1.Object
+// 2.Array
