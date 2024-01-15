147eb05a0abcfb38ee0d915ec54821c22f7c8e15
Roshan <sawantroshan98@gmail.com>
2024-01-15 12:05:27 +0530

    array_15

--- 02_basics/01_Array.js
+++ 02_basics/01_Array.js
@@ -19,3 +19,22 @@ const arr = [1,2,3,4,5,6]
 // console.log('arr = ', arr);
 // console.log(arr.splice(1,3));
 // console.log('arr = ', arr);
+
+
+// console.log(Array.isArray("Roshan"));
+// console.log(Array.from("Roshan"));
+// console.log(Array.of("Roshan"));
+
+const Bollywood = ['Hritik', 'Ranvir', 'Amir']
+const Tollywood = ['Yash', 'Rajanikant', 'AlluArjun']
+// Bollywood.push(Tollywood)
+// console.log(Bollywood);
+
+// const AllStars = Bollywood.concat(Tollywood)
+// console.log(AllStars);
+
+// AllStars = [...Tollywood, ...Bollywood]
+// console.log(AllStars);
+
+const numArr = [1,2,3, [5,6,[9,0],7],4]
+console.log(numArr.flat(3))
