23d889f2a40a46c659452885ee8adb2740e06621
Roshan <sawantroshan98@gmail.com>
2024-01-15 11:33:35 +0530

    BASIC02_array

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,8 @@
+// console.log(Date());
+const myDate = new Date('01-10-2023')
+// console.log(myDate);
+const myTimestamp = Date.now();
+console.log(myTimestamp);
+console.log(myDate.getTime());
+
+//In date method of javascript months strats fron 0 (january=='0')
---
+++ 02_basics/01_Array.js
@@ -0,0 +1,21 @@
+const myArray = [1,2,3,4,5]
+// myArray.push(6);
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.unshift(10);
+// console.log(myArray);
+// myArray.shift();
+// console.log(myArray);
+
+const newArray = new Array('a','b','c','d');
+// console.log(newArray.includes('c'));
+// console.log(newArray.indexOf('c'));
+
+// slice() VS splice()
+//splice takes (1,2) 1,2 and 3 also, and also manipulates original array
+const arr = [1,2,3,4,5,6]
+// console.log(arr.slice(1,3));
+// console.log('arr = ', arr);
+// console.log(arr.splice(1,3));
+// console.log('arr = ', arr);
